245c006073bf5e93c05cc6505bc57cb763dc0557
ZTakeshi <lucastk1210@gmail.com>
2019-03-07 03:18:01 -0300

    Adicionando a classe model do usuario

---
+++ Concessionaria_AutoCar/src/AC_MODEL/AC_MDUSUARIO.java
@@ -0,0 +1,93 @@
+package AC_MODEL;
+
+/**
+ *
+ * @author LucasxTakeshi
+ */
+public class AC_MDUSUARIO {
+    
+    private int idusuario;
+    private String nome;
+    private String email;
+    private String CPF;
+    private String telefone;
+    private String RG;
+    private String endereco;
+    private String login;
+    private String senha;
+
+    public int getIdusuario() {
+        return idusuario;
+    }
+
+    public void setIdusuario(int idusuario) {
+        this.idusuario = idusuario;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCPF() {
+        return CPF;
+    }
+
+    public void setCPF(String CPF) {
+        this.CPF = CPF;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getRG() {
+        return RG;
+    }
+
+    public void setRG(String RG) {
+        this.RG = RG;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+     
+    
+    
+}
